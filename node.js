c5970ba26c49d7ff10473ef10f599976912be55b
Pardheevsai03 <134611261+Pardheevsai03@users.noreply.github.com>
2024-03-08 14:51:31 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,52 +1,50 @@
-//importing the required modules and libraries
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const tedious = require('tedious');
-
+const express = require("express");
 const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
+const mysql = require("mysql");
 
-const config = {
-    authentication: {
-        type: 'default',
-        options: {
-            userName: 'newuser',
-            password: 'Hello1234'
-        }
-    },
-    server: 'Test1',
-    options: {
-        database: 'Test1',
-        encrypt: true
-    }
-};
-
-const connection = new tedious.Connection(config);
-
-connection.on('connect', function(err) {
-    if (err) {
-        console.error('Error connecting to SQL Server:', err);
-    } else {
-        console.log('Connected to SQL Server');
-    }
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "newuser",
+  password: "Hello1234",
+  database: "test1"
 });
 
-io.on('connection', function(socket) {
-    console.log('Client connected');
+db.connect(err => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL database!");
+});
 
-    socket.on('chatMessage', function(message) {
-        connection.execSql(new tedious.Request(`INSERT INTO ChatMessages (message) VALUES ('${message}')`, function(err) {
-            if (err) {
-                console.error('Error inserting message into SQL Server:', err);
-            } else {
-                console.log('Message inserted into SQL Server');
-                io.emit('chatMessage', message);
-            }
-        }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+server.listen(3000, () => console.log('Server started at port 3000'));
+
+app.use(express.static("./form.html"));
+
+app.get('/', (req, res) => {
+  return res.sendFile(__dirname + "./form.html");
+});
+
+io.on('connection', (socket) => {
+  console.log("ID of", socket.id, "is connected");
+  socket.on('chatMessage', (message) => {
+    const sql = 'INSERT INTO messages (message) VALUES (?)';
+    db.query(sql, [message], (err, result) => {
+      if (err) throw err;
+      console.log("Message saved to database");
     });
+    
+    io.emit('message', message);
+  });
 });
-
-const PORT = process.env.PORT || 5500;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
